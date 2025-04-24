85d16b3eb7754f021296b2be124daa1af4fa7556
Aditya kumar <adityakr80510@gmail.com>
2025-04-24 09:49:50 +0530

    update

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->string('password');
             $table->string('first_name');
-            $table->string('last_name');
+            $table->string('last_name')->nullable();
             $table->string('phone');
             $table->string('user_type')->nullable();;
             $table->string('preferred_contact')->nullable();
--- routes/properties.php
+++ routes/properties.php
@@ -4,9 +4,18 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Properties\UserController;
 use App\Http\Controllers\Properties\PropertyController;
 use App\Http\Controllers\Properties\DealController;
+use App\Http\Controllers\Properties\AgentController;
+use App\Http\Controllers\Properties\OwnerController;
+use App\Http\Controllers\Properties\BuyerController;
 
 Route::resource('/api/users', UserController::class);
 
 Route::resource('/api/properties', PropertyController::class);
 
 Route::resource('/api/deals', DealController::class);
+
+Route::resource('/api/agents', AgentController::class);
+
+Route::resource('/api/owners', OwnerController::class);
+
+Route::resource('/api/buyers', BuyerController::class);
